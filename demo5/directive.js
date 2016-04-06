35350e2e1485af020e7d781329494ee0dd7788a7
Roonie1 <wangliuzhan163@163.com>
2016-04-06 18:40:11 +0800

    modify

--- demo5/directive.js
+++ demo5/directive.js
@@ -6,15 +6,18 @@ var app = angular.module('directive',[])
         return {
             restrict:'E',
             templateUrl:'tpl.html',
+            require:'?^accordion',
             replace:true,
             transclude:true,
             scope:{
                 mytitle:'=etitle'
             },
-            link: function(scope,element,attr){
+            link: function(scope,element,attr,accordionCtrl){
                 scope.showText = false;
+                accordionCtrl.addExpander(scope);
                 scope.toggleText = function(){
                     scope.showText = ! scope.showText;
+                    accordionCtrl.getOpened(scope);
                 }
             }
         }
@@ -22,4 +25,29 @@ var app = angular.module('directive',[])
     app.controller('directive',function($scope){
         $scope.title = "个人简介";
         $scope.text = "Angular js概念多"
+    });
+
+
+    app.directive('accordion',function(){
+       return {
+           restrict:"E",
+           template:'<div ng-transclude></div>',
+           replace:true,
+           transclude:true,
+           controller:function(){
+               var expanders = [];
+
+               this.getOpened = function(selectExpander){
+                   angular.forEach(expanders,function(e){
+                        if (selectExpander != e){
+                            e.showText = false;
+                        }
+                   });
+               }
+
+               this.addExpander = function(e){
+                   expanders.push(e);
+               }
+           }
+       }
     });
