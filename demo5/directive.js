90bff0f4608ac80d1adc09c17bd990ed7f3b4d71
Roonie1 <wangliuzhan163@163.com>
2016-04-08 18:08:43 +0800

    modify

--- demo5/directive.js
+++ demo5/directive.js
@@ -1,55 +1,58 @@
 /**
  * Created by Administrator on 2016/4/6 0006.
  */
-var app = angular.module('directive',[])
-   app.directive('expander',function(){
-        return {
-            restrict:'E',
-            templateUrl:'tpl.html',
-            require:'?^accordion',
-            replace:true,
-            transclude:true,
-            scope:{
-                mytitle:'=etitle'
-            },
-            link: function(scope,element,attr,accordionCtrl){
-                scope.showText = false;
-                accordionCtrl.addExpander(scope);
-                scope.toggleText = function(){
-                    scope.showText = ! scope.showText;
-                    accordionCtrl.getOpened(scope);
-                }
-            }
-        }
-    });
-    app.controller('directive',function($scope){
-        $scope.expanders = [
-            {title:"angular",text:"angular js概念多"},
-            {title:"react",text:"react + reduce + ui路由机制"}
-        ];
-    });
-
-
-    app.directive('accordion',function(){
-       return {
-           restrict:"E",
-           template:'<div ng-transclude></div>',
-           replace:true,
-           transclude:true,
-           controller:function(){
-               var expanders = [];
-
-               this.getOpened = function(selectExpander){
-                   angular.forEach(expanders,function(e){
-                        if (selectExpander != e){
-                            e.showText = false;
-                        }
-                   });
-               }
-
-               this.addExpander = function(e){
-                   expanders.push(e);
-               }
-           }
-       }
-    });
+var app = angular.module('directive',[]);
+app.directive('expander',function(){
+    return {
+        restrict:'A',
+        template:'<div><span style="background-color:red;">{{mytitle}}</span><div ng-transclude></div></div>',
+        require:true,
+        replace:true,
+        transclude:true,
+        //将etitle属性绑定到父控制器的scope域中
+        scope:{
+            mytitle:'=etitle'
+        },
+    }
+});
+app.controller('directive',function($scope){
+    $scope.title = "学习";
+    $scope.text = "angular js概念多";
+});
+//
+//
+//app.directive('accordion',function(){
+//link: function(scope,element,attr,accordionCtrl){
+//    scope.showText = false;
+//    accordionCtrl.addExpander(scope);
+//    scope.toggleText = function(){
+//        scope.showText = ! scope.showText;
+//        accordionCtrl.getOpened(scope);
+//    }
+//}
+//$scope.expanders = [
+//    {title:"angular",text:"angular js概念多"},
+//    {title:"react",text:"react + reduce + ui路由机制"}
+//];
+//   return {
+//       restrict:"E",
+//       template:'<div ng-transclude></div>',
+//       replace:true,
+//       transclude:true,
+//       controller:function(){
+//           var expanders = [];
+//
+//           this.getOpened = function(selectExpander){
+//               angular.forEach(expanders,function(e){
+//                    if (selectExpander != e){
+//                        e.showText = false;
+//                    }
+//               });
+//           }
+//
+//           this.addExpander = function(e){
+//               expanders.push(e);
+//           }
+//       }
+//   }
+//});
